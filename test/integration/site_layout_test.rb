8015a1fc50bba94d99d28a8305c15e6315eb6ac3
shk33 <m.coronel.seg@gmail.com>
2014-12-07 16:44:26 -0600

    Add signup page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,10 +5,15 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layouts link" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+  end
+
+  test "signup button" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a.btn[href=?]", signup_path
   end
 
 end
