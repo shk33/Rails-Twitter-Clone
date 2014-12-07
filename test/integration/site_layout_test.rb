5a1284e3a80a2b32b7171e149eba288eb6f03c4c
shk33 <m.coronel.seg@gmail.com>
2014-12-07 16:51:38 -0600

    Add Application helper to Test Helper

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,12 +8,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a.btn[href=?]", signup_path
+
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 
-  test "signup button" do
-    get root_path
-    assert_template 'static_pages/home'
-    assert_select "a.btn[href=?]", signup_path
-  end
 
 end
