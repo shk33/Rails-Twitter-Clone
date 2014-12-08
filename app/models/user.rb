58629d94b58e20a207eae1f8953aa250479db453
shk33 <m.coronel.seg@gmail.com>
2014-12-07 19:17:56 -0600

    Add user email validation and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
-  validates :name,  presence: true
-  validates :email, presence: true
+  # Regexs for validatiosn
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  #Model validations
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
