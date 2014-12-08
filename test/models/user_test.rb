98c2600fbe89026568d0f2303a0fe524620e4631
shk33 <m.coronel.seg@gmail.com>
2014-12-07 18:34:34 -0600

    Add User model test validating length of email and name

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,12 +6,27 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "should be valid" do
-      assert @user.valid?
+    assert @user.valid?
   end
 
   test "name should be valid" do
-      @user.name = '       '
-      assert_not @user.valid?
+    @user.name = '       '
+    assert_not @user.valid?
+  end
+
+  test "email should be valid" do
+    @user.email = '       '
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a"*51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a"*256
+    assert_not @user.valid?
   end
 
 end
