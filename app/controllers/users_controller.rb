512f44e0f4303c2283adbb04f7d904002c7820bc
shk33 <m.coronel.seg@gmail.com>
2014-12-09 17:28:44 -0600

    User signup and test integration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome aboard"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
+
 end
