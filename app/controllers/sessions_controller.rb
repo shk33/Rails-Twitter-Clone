634ce09ed7fe2c0b6249531e3a9838de15a818ae
shk33 <m.coronel.seg@gmail.com>
2014-12-10 16:23:59 -0600

    Invalid login credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+
+    if user && user.authenticate(params[:session][:password])
+      #redirct the user to show profile  
+    else
+      #Create an error mesage
+      flash[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
+  end
+
+  def destroy
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
   root 'static_pages#home'
+
+  #Static Pages Routes
   get  'help'    => 'static_pages#help'
   get  'about'   => 'static_pages#about'
   get  'contact' => 'static_pages#contact'
+  
+  #Users
   get  'signup'  => 'users#new'
-
   resources :users
+
+  #Sessions
+  get    'login' => 'sessions#new'
+  post   'login' => 'sessions#create'
+  delete 'login' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
