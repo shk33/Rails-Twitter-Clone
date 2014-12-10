b8fbfb34efc1e1f7913c86ed0695de4f8a0e97e3
shk33 <m.coronel.seg@gmail.com>
2014-12-10 17:51:11 -0600

    Add Logge-din and Logged-out headers

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   #Sessions
   get    'login' => 'sessions#new'
   post   'login' => 'sessions#create'
-  delete 'login' => 'sessions#destroy'
+  delete 'logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
