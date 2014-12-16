ac99e003ff3548ef760f1fbd5089ec418ba08797
shk33 <m.coronel.seg@gmail.com>
2014-12-15 19:40:53 -0600

    Improve login with remembering test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
+    @user = User.find_by email: params[:session][:email].downcase
 
-    if user && user.authenticate(params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password])
       #redirct the user to show profile 
-      login user
+      login @user
       if params[:session][:remember_me] == '1'
-        remember user
+        remember @user
       else
-        forget user
+        forget @user
       end
-      redirect_to user 
+      redirect_to @user 
     else
       #Create an error mesage
       flash.now[:danger] = "Invalid email/password combination"
