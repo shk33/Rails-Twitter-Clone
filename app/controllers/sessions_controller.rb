fefa4829e02e3106d2017132e1b85c8d52a3d661
shk33 <m.coronel.seg@gmail.com>
2014-12-21 13:22:37 -0600

    I really do not remember what I added here

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
       else
         forget @user
       end
-      redirect_to @user 
+      redirect_back_or @user 
     else
       #Create an error mesage
       flash.now[:danger] = "Invalid email/password combination"
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -22,12 +22,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'sessions/new'
     post login_path, session: {email: @user.email, password: "password"}
     assert is_logged_in?
-    assert_redirected_to @user
+    assert_redirected_to user_path @user
     follow_redirect!
     assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
