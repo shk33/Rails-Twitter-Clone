d0403d2dc104e5795c239a19f194d78f55a68aae
shk33 <m.coronel.seg@gmail.com>
2014-12-24 18:03:09 -0600

    Add Activation fields to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141222065420) do
+ActiveRecord::Schema.define(version: 20141225000201) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20141222065420) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin"
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
