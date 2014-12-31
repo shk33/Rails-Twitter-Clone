3c9a16322d560cf0f4dc0a37ef1e7078b1738056
shk33 <m.coronel.seg@gmail.com>
2014-12-31 17:24:18 -0600

    Add create microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
+
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
@@ -10,4 +12,5 @@ class StaticPagesController < ApplicationController
   
   def contact
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,14 +60,6 @@ class UsersController < ApplicationController
       redirect_to root_url unless current_user.admin?
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
