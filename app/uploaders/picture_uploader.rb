58ebc086e746d1c32686d864e8bebcda96347a0e
shk33 <m.coronel.seg@gmail.com>
2014-12-31 19:17:06 -0600

    COnfiguring for upload store in AWS

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,6 +7,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
